b5eb959c8079bf9194638ff4aaa195c5d7a05b0b
SeyedAli <seyed.ali.devl@gmail.com>
2025-03-19 12:54:34 +0330

    initial commit

---
+++ backend/freetoolsy-server/src/main/java/com/example/freetoolsy_server/FreetoolsyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.freetoolsy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetoolsyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreetoolsyServerApplication.class, args);
+	}
+
+}
---
+++ backend/freetoolsy-server/src/test/java/com/example/freetoolsy_server/FreetoolsyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freetoolsy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreetoolsyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
